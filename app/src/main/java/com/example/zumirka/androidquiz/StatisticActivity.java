1768ed8ed42e9295b0bcbf08abb83133e7e0479e
Zumirka <dominika.bartek1207@gmail.com>
2018-01-20 00:24:53 +0100

    StatisticActivity code clean

--- app/src/main/java/com/example/zumirka/androidquiz/StatisticActivity.java
+++ app/src/main/java/com/example/zumirka/androidquiz/StatisticActivity.java
@@ -6,9 +6,9 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -36,62 +36,80 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import java.util.ArrayList;
 
 
-
 public class StatisticActivity extends AppCompatActivity implements
         OnChartGestureListener,
         OnChartValueSelectedListener {
 
 
-
-    String userName="",time1,time2,selectedGrid="";
-    ArrayList<Statistic> statisticsList=new ArrayList<>();
-    ArrayList<TextView> columns=new ArrayList<>();
+    String userName = "", time1, time2;
+    ArrayList<Statistic> statisticsList = new ArrayList<>();
     TableLayout l;
-    ImageButton fromDate,toDate;
+    ImageButton fromDate, toDate;
     Calendar cal;
-    int day,month,year,idDialog=1;
-    EditText fromDateTxt,toDateTxt;
-    TableRow tr,trl;
+    int day, month, year;
+    EditText fromDateTxt, toDateTxt;
+    TableRow tr, trl;
     ArrayList<Entry> entries;
-    ArrayList<String> PieEntryLabels;
-    int CorrectAnswear=0,PointsCount=0;
+    ArrayList<String> pieEntryLabels;
+    int correctAnswear = 0, pointsCount = 0;
     LineChart lineChart;
     TextView emptyTableTxt;
+    int buttonId;
 
+    public DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+
+            if (buttonId == fromDate.getId()) {
+                fromDateTxt.setText(String.format("%02d", selectedDay) + "-" + String.format("%02d", selectedMonth + 1) + "-" + String.format("%04d", selectedYear));
+                time1 = String.format("%04d", selectedYear) + "-" + String.format("%02d", selectedMonth + 1) + "-" + String.format("%02d", selectedDay);
+            }
+            if (buttonId == toDate.getId()) {
+                toDateTxt.setText(String.format("%02d", selectedDay) + "-" + String.format("%02d", selectedMonth + 1) + "-" + String.format("%04d", selectedYear));
+                time2 = String.format("%04d", selectedYear) + "-" + String.format("%02d", selectedMonth + 1) + "-" + String.format("%02d", selectedDay);
+                TakeStatistic();
+            }
+
+        }
+    };
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
         SharedPreferences resSettings = getSharedPreferences("BYLECO", MODE_PRIVATE);
+        initGUI(resSettings);
+        setData();
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void initGUI(SharedPreferences resSettings) {
         userName = resSettings.getString("USER_NAME", "empty");
         l = (TableLayout) findViewById(R.id.table);
-        fromDate=(ImageButton) findViewById(R.id.fromDate);
-        toDate=(ImageButton) findViewById(R.id.toDate);
-        fromDateTxt=findViewById(R.id.fromDateTxt);
-        toDateTxt=findViewById(R.id.toDateTxt);
-        cal=Calendar.getInstance();
-        year=cal.get(Calendar.YEAR);
-        month=cal.get(Calendar.MONTH );
-        day=cal.get(Calendar.DAY_OF_MONTH);
+        fromDate = (ImageButton) findViewById(R.id.fromDate);
+        toDate = (ImageButton) findViewById(R.id.toDate);
+        fromDateTxt = findViewById(R.id.fromDateTxt);
+        toDateTxt = findViewById(R.id.toDateTxt);
+        cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH);
+        day = cal.get(Calendar.DAY_OF_MONTH);
         lineChart = (LineChart) findViewById(R.id.linechart);
         lineChart.setOnChartGestureListener(this);
         lineChart.setOnChartValueSelectedListener(this);
-        setData();
-        Legend l =lineChart.getLegend();
+        Legend l = lineChart.getLegend();
         l.setForm(Legend.LegendForm.LINE);
         emptyTableTxt = findViewById(R.id.EmptyTableTxt);
-
-
     }
-    int buttonId;
-    public void onClickV(View v)
-    {
-        buttonId=v.getId();
+
+    public void onClickV(View v) {
+        buttonId = v.getId();
         showDialog(0);
     }
+
     @Override
     @Deprecated
     protected Dialog onCreateDialog(int id) {
@@ -99,129 +117,113 @@ public class StatisticActivity extends AppCompatActivity implements
         return new DatePickerDialog(this, datePickerListener, year, month, day);
     }
 
-    public DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int selectedYear,
-                              int selectedMonth, int selectedDay) {
+    // TODO type
+    private void TakeStatistic() {
 
-            year=selectedYear;
-            month=selectedMonth;
-            month++;
-            day=selectedDay;
-            if(buttonId==fromDate.getId()){
-            fromDateTxt.setText(String.format("%02d",day) + "-" +String.format("%02d", month)  + "-" +String.format("%04d", year));
-            time1=String.format("%04d", year)+"-"+String.format("%02d", month)+"-"+String.format("%02d",day);
-            }
-            if(buttonId==toDate.getId())
-            {
-                toDateTxt.setText(String.format("%02d",day) + "-" +String.format("%02d", month)  + "-" +String.format("%04d", year));
-                time2=String.format("%04d", year)+"-"+String.format("%02d", month)+"-"+String.format("%02d",day);
-                TakeStatistic();
-            }
-
-        }
-    };
-
-
-    private void TakeStatistic()
-    {
-
-        String type="getStats";
+        String type = "getStats";
         getStatisticBackgroundWorker getStatisticBackgroundWorker = new getStatisticBackgroundWorker(this);
-        getStatisticBackgroundWorker.execute(type,userName,time1,time2);
+        getStatisticBackgroundWorker.execute(type, userName, time1, time2);
         l.removeAllViewsInLayout();
     }
+
     public void setCategory(ArrayList<Statistic> stats) {
         this.statisticsList = stats;
-        if(stats.size()!=0) {
+        if (stats.size() != 0) {
             emptyTableTxt.setText("");
-            trl = new TableRow(this);
-            TextView labels = new TextView(this);
-            labels.setText("Kategoria");
-            labels.setGravity(Gravity.CENTER);
-            trl.addView(labels);
-            labels = new TextView(this);
-            labels.setText("Poziom trudności");
-            labels.setGravity(Gravity.CENTER);
-            trl.addView(labels);
-            labels = new TextView(this);
-            labels.setText("Czas");
-            labels.setGravity(Gravity.CENTER);
-            trl.addView(labels);
-            labels = new TextView(this);
-            labels.setText("Punkty");
-            labels.setGravity(Gravity.CENTER);
-            trl.addView(labels);
-            labels = new TextView(this);
-            labels.setText("Data");
-            labels.setGravity(Gravity.CENTER);
-            trl.addView(labels);
-            trl.setBackgroundColor(Color.GRAY);
-            trl.setPadding(1, 4, 1, 3);
+            initTableHeader();
             l.addView(trl);
 
             for (int i = 0; i < statisticsList.size(); i++) {
-                tr = new TableRow(this);
-                TextView txt = new TextView(this);
-                txt.setText(statisticsList.get(i).getCategory().getName());
-                txt.setGravity(Gravity.CENTER);
-                tr.addView(txt);
-                txt = new TextView(this);
-                txt.setText(statisticsList.get(i).getDifficulty());
-                txt.setGravity(Gravity.CENTER);
-                tr.addView(txt);
-                txt = new TextView(this);
-                txt.setText(statisticsList.get(i).getTime());
-                txt.setGravity(Gravity.CENTER);
-                tr.addView(txt);
-                txt = new TextView(this);
-                txt.setText(statisticsList.get(i).getPoints());
-                txt.setGravity(Gravity.CENTER);
-                tr.addView(txt);
-                txt = new TextView(this);
-                txt.setText(statisticsList.get(i).getDate());
-                txt.setGravity(Gravity.CENTER);
-                tr.addView(txt);
-                tr.setBackgroundColor(Color.LTGRAY);
-                tr.setPadding(2, 4, 3, 3);
+                insertData(i);
                 l.addView(tr);
                 tr.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         entries = new ArrayList<>();
-                        PieEntryLabels = new ArrayList<String>();
+                        pieEntryLabels = new ArrayList<String>();
                         TableRow clickRow = (TableRow) view;
                         TextView firstTextView = (TextView) clickRow.getChildAt(1);
                         TextView secondTextView = (TextView) clickRow.getChildAt(3);
                         String firstText = firstTextView.getText().toString();
                         if (firstText == "Łatwy") {
-                            PointsCount = 10;
+                            pointsCount = 10;
                         }
                         if (firstText == "Średni") {
-                            PointsCount = 20;
+                            pointsCount = 20;
                         }
                         if (firstText == "Trudny") {
-                            PointsCount = 30;
+                            pointsCount = 30;
                         }
-                        CorrectAnswear = Integer.parseInt(secondTextView.getText().toString());
+                        correctAnswear = Integer.parseInt(secondTextView.getText().toString());
 
 
                     }
                 });
 
             }
-        }
-        else
-        {
-        //    l.removeAllViewsInLayout();
+        } else {
             l.removeAllViews();
             emptyTableTxt.setText("Brak statystyk");
-          emptyTableTxt.setGravity(Gravity.CENTER);
+            emptyTableTxt.setGravity(Gravity.CENTER);
 
         }
 
     }
+
+    private void insertData(int i) {
+        tr = new TableRow(this);
+        TextView txt = new TextView(this);
+        txt.setText(statisticsList.get(i).getCategory().getName());
+        txt.setGravity(Gravity.CENTER);
+        tr.addView(txt);
+        txt = new TextView(this);
+        txt.setText(statisticsList.get(i).getDifficulty());
+        txt.setGravity(Gravity.CENTER);
+        tr.addView(txt);
+        txt = new TextView(this);
+        txt.setText(statisticsList.get(i).getTime());
+        txt.setGravity(Gravity.CENTER);
+        tr.addView(txt);
+        txt = new TextView(this);
+        txt.setText(statisticsList.get(i).getPoints());
+        txt.setGravity(Gravity.CENTER);
+        tr.addView(txt);
+        txt = new TextView(this);
+        txt.setText(statisticsList.get(i).getDate());
+        txt.setGravity(Gravity.CENTER);
+        tr.addView(txt);
+        tr.setBackgroundColor(Color.LTGRAY);
+        tr.setPadding(2, 4, 3, 3);
+    }
+
+    private void initTableHeader() {
+        trl = new TableRow(this);
+        TextView labels = new TextView(this);
+        labels.setText("Kategoria");
+        labels.setGravity(Gravity.CENTER);
+        trl.addView(labels);
+        labels = new TextView(this);
+        labels.setText("Poziom trudności");
+        labels.setGravity(Gravity.CENTER);
+        trl.addView(labels);
+        labels = new TextView(this);
+        labels.setText("Czas");
+        labels.setGravity(Gravity.CENTER);
+        trl.addView(labels);
+        labels = new TextView(this);
+        labels.setText("Punkty");
+        labels.setGravity(Gravity.CENTER);
+        trl.addView(labels);
+        labels = new TextView(this);
+        labels.setText("Data");
+        labels.setGravity(Gravity.CENTER);
+        trl.addView(labels);
+        trl.setBackgroundColor(Color.GRAY);
+        trl.setPadding(1, 4, 1, 3);
+    }
+
     //set x value on chart
-    private ArrayList<String> setXAxisValues(){
+    private ArrayList<String> setXAxisValues() {
         ArrayList<String> xVals = new ArrayList<String>();
         xVals.add("10");
         xVals.add("20");
@@ -231,8 +233,9 @@ public class StatisticActivity extends AppCompatActivity implements
 
         return xVals;
     }
+
     //set y value on chart
-    private ArrayList<Entry> setYAxisValues(){
+    private ArrayList<Entry> setYAxisValues() {
         ArrayList<Entry> yVals = new ArrayList<Entry>();
         yVals.add(new Entry(60, 0));
         yVals.add(new Entry(48, 1));
@@ -242,6 +245,7 @@ public class StatisticActivity extends AppCompatActivity implements
 
         return yVals;
     }
+
     private void setData() {
         ArrayList<String> xVals = setXAxisValues();
 
@@ -272,12 +276,13 @@ public class StatisticActivity extends AppCompatActivity implements
         LineData data = new LineData(xVals, dataSets);
 
         // set data
-       lineChart.setData(data);
-      // lineChart.setTouchEnabled(false);
-       lineChart.setDragEnabled(false);
+        lineChart.setData(data);
+        // lineChart.setTouchEnabled(false);
+        lineChart.setDragEnabled(false);
         lineChart.setScaleEnabled(false);
 
     }
+
     @Override
     public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
         Log.i("Gesture", "START, x: " + me.getX() + ", y: " + me.getY());
